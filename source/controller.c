ceb39033d2c11877ce3e4c71d6aa45b6b8ea5d26
Saim Iqbal <saim.iqbal@shifthyperloop.com>
2022-03-01 15:23:26 +0100

    clear button lights when clearing queue

--- source/controller.c
+++ source/controller.c
@@ -28,8 +28,11 @@ void controller_removeFloorOrder(int floor) {
 }
 
 void controller_emptyFloorOrders() {
-    for (int i = 0; i < N_FLOORS; i++) {
-        m_floorOrders[i] = NO_ORDER;
+    for (int f = 0; f < N_FLOORS; f++) {
+        m_floorOrders[f] = NO_ORDER;
+        for (int b = 0; b < N_BUTTONS; b++) {
+            elevio_buttonLamp(f, b, 0);
+        }
     }
 }
 
@@ -68,7 +71,6 @@ void controller_addFloorOrder(int requestedFloor, ButtonType btnType) {
                 break;
         }
     }
-
     m_floorOrders[requestedFloor] = order;
     printf("Order: %d \n", (int)order);
 }
